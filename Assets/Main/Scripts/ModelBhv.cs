48843d6f7eb5286a3d1af428807734df1ca399c2
ru-ju <38347528+ru-ju@users.noreply.github.com>
2020-02-10 13:15:26 +0900

    モデル切り替え実装

---
+++ Assets/Main/Scripts/ModelBhv.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelBhv : MonoBehaviour
+{
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+
+    public bool Show()
+    {
+        if (gameObject.activeSelf)
+        {
+            return false;
+        }
+        else
+        {
+            gameObject.SetActive(true);
+            return true;
+        }
+    }
+
+    public bool Hide()
+    {
+        if (!gameObject.activeSelf)
+        {
+            return false;
+        }
+        else
+        {
+            gameObject.SetActive(false);
+            return true;
+        }
+    }
+}
---
+++ Assets/Main/Scripts/ModelChangeMng.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelChangeMng : MonoBehaviour
+{
+    //modelsには、同時に出現させたいやつをまとめて空オブジェクトの子とかにしてその空オブジェクトをいれる
+    [SerializeField]
+    private GameObject[] models;
+
+    [SerializeField]
+    private int init_model;
+
+    private int now_model;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        now_model = init_model;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ChangeModel(1);
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            ChangeModel(0);
+        }
+        
+    }
+
+    //boolにしてもいいけど意味なさげ
+    public void ChangeModel(int model_num)
+    {
+        //いらんかった
+        if (models[now_model].GetComponent<ModelBhv>().Hide())
+        {
+            
+        }
+
+        
+        if (models[model_num].GetComponent<ModelBhv>().Show())
+        {
+            now_model = model_num;
+        }
+    }
+}
